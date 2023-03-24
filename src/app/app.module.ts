82577f90766d4ce47abf33aea1231eb0e6053f1c
Ivan <ivangalvan93@gmail.com>
2023-03-24 16:37:15 -0300

    Restore

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExpEduComponent } from './componentes/edu/exp-edu.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { NewExperienciaComponent } from './componentes/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './componentes/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './componentes/edu/neweducacion.component';
+import { EditeducacionComponent } from './componentes/edu/editeducacion.component';
+import { EditSkillComponent } from './componentes/skills/edit-skill.component';
+import { NewSkillComponent } from './componentes/skills/new-skill.component';
+import { EditAcercaDeComponent } from './componentes/acerca-de/edit-acerca-de.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExpEduComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    HomeComponent,
+    LoginComponent,
+    ExperienciaComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent,
+    EditSkillComponent,
+    NewSkillComponent,
+    EditAcercaDeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [
+    interceptorProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/service/persona.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { persona } from '../model/persona.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonaService {
+  URL = 'http://localhost:8080/personas/';
+  //URL = 'https://frontap-f0b8b.web.app/personas/'
+
+  constructor(private httpClient: HttpClient) { }
+
+  public lista(): Observable<persona[]>{
+    return this.httpClient.get<persona[]>(this.URL + 'lista');
+  }
+
+  public detail(id: number): Observable<persona>{
+    return this.httpClient.get<persona>(this.URL + `detail/${id}`);
+  }
+
+
+  public update(id: number, Persona: persona): Observable<any>{
+    return this.httpClient.put<any>(this.URL + `update/${id}`, Persona);
+  }
+
+  
+}
